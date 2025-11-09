47b63b08ed8abc2675ae8b0e022d865cb33205cb
Opirajbhor <opiraj6@gmail.com>
2025-11-09 20:29:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-4xl text-red-500'>
+        hello
+      </div>
+    </>
+  )
+}
+
+export default App
